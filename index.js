246b2e565dfee1cd9666488f1a18d9016a403b61
Chepayne96 <payne_cheyenne@outlook.com>
2024-03-19 23:14:52 -0400

    Finished the playRound function started work on the playGame function

--- index.js
+++ index.js
@@ -1,52 +1,53 @@
 let choiceArr = ["rock", "paper", "scissor"];
 // getComputerChoice --> randomly pick from list for the pc
-let computerSelection = "";
-function getComputerChoice() {
+let computerSelection;
+function getComputerChoice(computerSelection) {
     let i = Math.floor(Math.random() * 3);
-    let computerSelection = choiceArr[i];
+     computerSelection = choiceArr[i];
         // console.log(computerSelection);
     return computerSelection.toLowerCase();
 }
+
+
 // Get the input from the player (prompt)
-let playerSelection = "";
-function getPlayerChoice() {
-    let playerSelection = prompt("Rock, Paper, Scissor: ");
+let playerSelection;
+function getPlayerChoice(playerSelection) {
+    playerSelection = prompt("Rock, Paper, Scissor: ");
     return playerSelection.toLowerCase();
 }
+
+
 // Compare the choice from player to pc to find winner
 function playRound() {
-    // for (i = 0; i <= 5; i++) {
-        getComputerChoice();
-        getPlayerChoice();
-        switch (playerSelection, computerSelection) {
-            case playerSelection === 'rock' && computerSelection === 'rock':
-                console.log('You have tied!!');
-                break;
-            case playerSelection === 'rock' && computerSelection === 'paper':
-                console.log('You have lost. Paper beats Rock!!');
-                break;
-            case playerSelection === 'rock' && computerSelection === 'scissor':
-                console.log('You have won. Rock beats Scissor!!');
-                break;
-            case playerSelection === 'paper' && computerSelection === 'rock':
-                console.log('You have won. Paper beats Rock!!');
-                break;
-            case playerSelection === 'paper' && computerSelection === 'paper':
-                console.log('You have tied!!');
-                break;
-            case playerSelection === 'paper' && computerSelection === 'scissor':
-                console.log('You have lost. Scissor beats Paper!!');
-                break;
-            case playerSelection === 'scissor' && computerSelection === 'rock':
-                console.log('You have lost. Rock beats Scissor!!');
-                break;
-            case playerSelection === 'scissor' && computerSelection === 'paper':
-                console.log('You have won. Scissor beats Paper!!');
-                break;
-            case playerSelection === 'scissor' && computerSelection === 'scissor':
-                console.log('You have tied!!');
-                break;
-        }
-    // }
+    let cc = getComputerChoice();
+    let pc = getPlayerChoice();
+    console.log(pc, cc);
+    if (pc === 'rock' && cc === 'rock'){
+        console.log('Its a tie!');
+    } else if (pc === 'paper' && cc === 'rock'){
+        console.log('Paper beats rock. You win!!');
+    } else if (pc === 'scissor' && cc === 'rock'){
+        console.log('Rock beats scissors. You loss!!');
+    } else if (pc === 'rock' && cc === 'paper'){
+        console.log('Paper beats rock. You loss!!');
+    } else if (pc === 'paper' && cc === 'paper'){
+        console.log('Its a tie!!');
+    } else if (pc === 'scissor' && cc === 'paper'){
+        console.log('Scissor beats paper. You win!!');
+    } else if (pc === 'rock' && cc === 'scissor'){
+        console.log('Rock beats scissor. You win!!');
+    } else if (pc === 'paper' && cc === 'scissor'){
+        console.log('Scissor beats paper. You loss!!');
+    } else if (pc === 'scissor' && cc === 'scissor'){
+        console.log('Its a tie!!');
+    }
 }
-playRound();
+
+// playRound();
+
+// Call playRound 5 times and keep score of who wins
+function playGame() {
+    for (i = 0; i < 5; i++){
+        let round = playRound();
+    }
+}
